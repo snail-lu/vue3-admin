0a039d02b3b9f21580ce424d62829a6bdfd2ae45
Snail-Lu <snaillu@yeah.net>
2022-04-18 21:09:24 +0800

    按需引入Element-Plus

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+  <el-button type="primary">Primary</el-button>
   <router-view />
 </template>
 

7b4ab0f9c2a14aba1faa54ba6e99bde30ab0ce4e
Snail-Lu <snaillu@yeah.net>
2022-04-22 23:03:14 +0800

    新增页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">Not Found</div>
+</template>
+<script lang="ts">
+export default {
+  name: "HomeView",
+};
+</script>
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  align-items: center;
+}
+</style>
---
+++ src/views/goods/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>商品列表</h1>
+  </div>
+</template>

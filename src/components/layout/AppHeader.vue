0b8807d91aa9a894a56bdfe33bec1f0ad1e049da
Snail-Lu <snaillu@yeah.net>
2022-04-23 14:21:48 +0800

    Header中import修改

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -43,7 +43,7 @@
 import { computed, ref, reactive } from "vue";
 import { useRoute, useRouter } from "vue-router";
 // 只使用组件的api需要手动引入组件，样式已在main.ts中引入
-import { ElMessageBox, ElIcon } from "element-plus";
+import { ElMessageBox } from "element-plus";
 
 const route = useRoute();
 const key = computed(() => {

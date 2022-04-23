416cef0a8c21e8f2ced7b9c107e2739f7bfd7565
Snail-Lu <snaillu@yeah.net>
2022-04-23 15:21:48 +0800

    引入vuex

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { store } from "./store/index";
 import * as ElIcons from "@element-plus/icons-vue";
 import "element-plus/es/components/message/style/css";
 import "element-plus/es/components/message-box/style/css";
@@ -10,4 +11,5 @@ for (const name in ElIcons) {
   app.component(name, (ElIcons as any)[name]);
 }
 app.use(router);
+app.use(store);
 app.mount("#app");
---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+import { StateDto } from './../types/store'
+
+// 创建一个新的 store 实例
+export const store = createStore({
+  state () {
+    return {
+      isCollapse: false
+    }
+  },
+  mutations: {
+    changeCollapse(state: StateDto) {
+      state.isCollapse = !state.isCollapse
+    }
+  }
+})

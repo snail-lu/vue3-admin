cd4404e76d437cb56bbaacc56042ccaac2793fc7
Snail-Lu <snaillu@yeah.net>
2022-07-16 23:05:26 +0800

    图标组件全局注册

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ import 'element-plus/es/components/message/style/css';
 import 'element-plus/es/components/message-box/style/css';
 
 const app = createApp(App);
-// for (const name in ElIcons) {
-//     app.component(name, (ElIcons as any)[name]);
-// }
+for (const name in ElIcons) {
+    app.component(name, (ElIcons as any)[name]);
+}
 app.use(router);
 app.use(store);
 app.mount('#app');

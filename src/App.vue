8016b672e421656fe7950a54cfd58585df942a39
Snail-Lu <snaillu@yeah.net>
2022-07-17 23:02:36 +0800

    配置Element-Plus语言为中文

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <router-view />
+    <el-config-provider :locale="locale">
+        <router-view />
+    </el-config-provider>
 </template>
+
+<script>
+import { defineComponent } from 'vue';
+import { ElConfigProvider } from 'element-plus';
+
+import zhCn from 'element-plus/lib/locale/lang/zh-cn';
+
+export default defineComponent({
+    components: {
+        ElConfigProvider
+    },
+    setup() {
+        return {
+            locale: zhCn
+        };
+    }
+});
+</script>
 
 <style lang="scss">
 * {
-  padding: 0;
-  margin: 0;
+    padding: 0;
+    margin: 0;
 }
 
 html,
 body,
 #app {
-  width: 100%;
-  height: 100%;
+    width: 100%;
+    height: 100%;
 }
 </style>

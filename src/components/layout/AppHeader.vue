f0379c77e48ea69297f22c8e6bf28951f9857933
Snail-Lu <snaillu@yeah.net>
2022-07-18 14:15:13 +0800

    AppHeader修改

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,128 +1,127 @@
 <template>
-  <div class="header-container">
-    <!-- 系统标题 -->
-    <div class="box-l">
-      <img class="logo" src="../../assets/logo.png" />
-      <span class="title">商城管理系统</span>
-    </div>
+    <div class="header-container">
+        <!-- 系统标题 -->
+        <div class="box-l">
+            <img class="logo" src="../../assets/logo.png" />
+            <span class="title">商城管理系统</span>
+        </div>
 
-    <!-- 用户信息 -->
-    <div class="box-r">
-      <el-icon
-        class="fullscreen-icon"
-        :size="24"
-        color="#ffffff"
-        @click="handleFullScreen"
-        title="全屏"
-      >
-        <full-screen />
-      </el-icon>
-      <el-dropdown @command="logout" trigger="click">
-        <span class="user-name flex-box flex-v-center">
-          {{ userInfo.userName || ""
-          }}<el-icon>
-            <arrow-down />
-          </el-icon>
-        </span>
-        <template #dropdown>
-          <el-dropdown-menu>
-            <el-dropdown-item>退出登录</el-dropdown-item>
-          </el-dropdown-menu>
-        </template>
-      </el-dropdown>
+        <!-- 用户信息 -->
+        <div class="box-r">
+            <el-icon class="fullscreen-icon" :size="24" color="#ffffff" @click="handleFullScreen" title="全屏">
+                <full-screen />
+            </el-icon>
+            <el-dropdown @command="logout" trigger="click">
+                <div class="flex-box flex-v-center">
+                    Hi,
+                    <span class="user-name flex-box flex-v-center">
+                        {{ userInfo.userName || ''
+                        }}<el-icon>
+                            <arrow-down />
+                        </el-icon>
+                    </span>
+                </div>
+
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item>退出登录</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, reactive } from "vue";
-import { useRouter } from "vue-router";
+import { ref, reactive } from 'vue';
+import { useRouter } from 'vue-router';
 // 只使用组件的api需要手动引入组件，样式已在main.ts中引入
-import { ElMessageBox } from "element-plus";
+import { ElMessageBox } from 'element-plus';
 
 const userInfo = reactive({
-  userName: "admin",
+    userName: 'admin'
 });
 
 const isFullScreen = ref(false);
 
 // 全屏事件
 function handleFullScreen() {
-  let element = document.documentElement;
-  if (isFullScreen.value) {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
+    let element = document.documentElement;
+    if (isFullScreen.value) {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    } else {
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        }
     }
-  } else {
-    if (element.requestFullscreen) {
-      element.requestFullscreen();
-    }
-  }
-  isFullScreen.value = !isFullScreen.value;
+    isFullScreen.value = !isFullScreen.value;
 }
 
 const router = useRouter();
 // 登出
 function logout() {
-  ElMessageBox.confirm("确定退出登录吗", "提示", {
-    confirmButtonText: "确定",
-    cancelButtonText: "取消",
-    type: "warning",
-  }).then(
-    () => {
-      // 清空本地缓存，并重定向到登录页
-      localStorage.clear();
-      router.replace("/login");
-    },
-    () => {}
-  );
+    ElMessageBox.confirm('确定退出登录吗', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+    }).then(
+        () => {
+            // 清空本地缓存，并重定向到登录页
+            localStorage.clear();
+            router.replace('/login');
+        },
+        () => {}
+    );
 }
 </script>
 
 <style lang="scss" scoped>
 .header-container {
-  height: 60px;
-  // background-color: #242f42;
-  background-color: #01182a;
-  color: #fff;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  .box-l {
-    display: flex;
-    align-items: center;
-    padding-left: 20px;
-
-    .logo {
-      width: 32px;
-      height: 32px;
-      margin-right: 10px;
-    }
-
-    .title {
-      height: 60px;
-      line-height: 60px;
-      font-size: 20px;
-    }
-  }
-
-  .box-r {
-    display: flex;
-    align-items: center;
+    height: 60px;
+    // background-color: #242f42;
+    background-color: #01182a;
     color: #fff;
-    padding-right: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-    .fullscreen-icon {
-      margin-right: 20px;
-      cursor: pointer;
+    .box-l {
+        display: flex;
+        align-items: center;
+        padding-left: 20px;
+
+        .logo {
+            width: 32px;
+            height: 32px;
+            margin-right: 10px;
+        }
+
+        .title {
+            height: 60px;
+            line-height: 60px;
+            font-size: 20px;
+        }
     }
 
-    .user-name {
-      color: #fff;
-      cursor: pointer;
-      user-select: none;
+    .box-r {
+        display: flex;
+        align-items: center;
+        color: #fff;
+        padding-right: 20px;
+
+        .fullscreen-icon {
+            margin-right: 20px;
+            cursor: pointer;
+        }
+
+        .user-name {
+            color: rgb(14, 236, 133);
+            cursor: pointer;
+            user-select: none;
+            margin-left: 4px;
+        }
     }
-  }
 }
 </style>

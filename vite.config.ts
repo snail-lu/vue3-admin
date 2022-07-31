bd0da8c6e7f6a1b3e9fa116a7cc533cc617c52d7
Snail-Lu <snaillu@yeah.net>
2022-07-31 15:30:26 +0800

    配置代理

--- vite.config.ts
+++ vite.config.ts
@@ -48,7 +48,14 @@ export default defineConfig({
     },
     server: {
         host: '0.0.0.0',
-        port: 8080
+        port: 8080,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:4000',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '/api')
+            }
+        }
     },
     css: {
         postcss: {

d8a677a97597f4dc394122e7745605a5fa230f42
Snail-Lu <snaillu@yeah.net>
2022-07-31 15:31:01 +0800

    axios请求baseUrl该从环境变量中获取

--- src/request/index.ts
+++ src/request/index.ts
@@ -4,7 +4,7 @@ import 'element-plus/es/components/message/style/css';
 
 // 创建HTTP实例
 const request = axios.create({
-    baseURL: 'https://www.fastmock.site/mock/810bb3cba8677f9f01afe7855db87edb/vue3-ts-admin/api',
+    baseURL: import.meta.env.VITE_BASE_URL,
     timeout: 2000,
     headers: {
         'Content-Type': 'application/json;charset=utf-8'
@@ -25,7 +25,7 @@ request.interceptors.response.use(
     (res) => {
         const code: number = res.data.code;
         if (code !== 200) {
-            ElMessage.error(res.data.msg);
+            ElMessage.error(res.data.message);
             return Promise.reject(res.data);
         }
         return res.data;

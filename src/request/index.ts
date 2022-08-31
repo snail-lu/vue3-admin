42379d2c6c37961ea7c1a678158cebb06e724c14
Snail-Lu <snaillu@yeah.net>
2022-08-31 21:36:46 +0800

    网络请求超时时间设置为2min

--- src/request/index.ts
+++ src/request/index.ts
@@ -5,7 +5,7 @@ import 'element-plus/es/components/message/style/css';
 // 创建HTTP实例
 const request = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
-    timeout: 2000,
+    timeout: 120 * 1000,
     headers: {
         'Content-Type': 'application/json;charset=utf-8'
     }

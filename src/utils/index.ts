ef04517a772f88d2a1316939e21cda0fc3e4a282
Snail-Lu <snaillu@yeah.net>
2022-09-04 00:08:27 +0800

    新增utils工具方法文件

---
+++ src/utils/index.ts
@@ -0,0 +1,22 @@
+import Layout from '@/components/layout/index.vue';
+
+const modules = import.meta.glob('../views/**/*.vue');
+
+export const transformRoutes = (routes) => {
+    routes.forEach((route) => {
+        route.component = loadComponent(route.component);
+        // debugger;
+        if (route.children) {
+            transformRoutes(route.children);
+        }
+    });
+    return routes;
+};
+
+export const loadComponent = (component: string) => {
+    if (component === 'Layout') {
+        return Layout;
+    } else {
+        return modules[component];
+    }
+};

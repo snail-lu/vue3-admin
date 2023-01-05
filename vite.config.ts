ee544134d918a33cca86db3ad8ae0557ab79643e
Snail-Lu <snaillu@yeah.net>
2023-01-05 18:24:16 +0800

    feat: 移除elment-plus自动引入图标改为全局引入

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import Icons from 'unplugin-icons/vite';
-import IconsResolver from 'unplugin-icons/resolver';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
@@ -16,27 +14,13 @@ export default defineConfig({
             // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
             imports: ['vue'],
             // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
-            resolvers: [
-                ElementPlusResolver(),
-
-                // 自动导入图标组件
-                IconsResolver({
-                    prefix: 'Icon'
-                })
-            ]
+            resolvers: [ElementPlusResolver()]
         }),
         Components({
             resolvers: [
-                // 自动注册图标组件
-                IconsResolver({
-                    enabledCollections: ['ep']
-                }),
                 // 自动导入 Element Plus 组件
                 ElementPlusResolver()
             ]
-        }),
-        Icons({
-            autoInstall: true
         })
     ],
     resolve: {
